0223987150601d95dcc39c767cb2672e6a33b40d
Shift II <Shift II@DESKTOP-68OJHRR>
2020-10-07 16:00:46 +0530

    test

---
+++ Test Suites/BMJunction-Login-Logout.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BMJunction-Login-Logout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3322e131-be63-4092-b1ca-b34ced475da9</testSuiteGuid>
+   <testCaseLink>
+      <guid>02e4e5da-ad28-410a-959a-b3f0c9c50ff6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JunctionLogin-Testingsource</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>93d6da5e-efae-40eb-9b15-65950d7b6dad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JunctionLogOut</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
