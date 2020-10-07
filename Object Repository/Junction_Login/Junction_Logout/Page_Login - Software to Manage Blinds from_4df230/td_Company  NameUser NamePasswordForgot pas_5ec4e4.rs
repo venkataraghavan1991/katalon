364629580e4d84341330481f059fe128685178f6
Shift II <Shift II@DESKTOP-68OJHRR>
2020-10-07 16:01:02 +0530

    test

---
+++ Object Repository/Junction_Login/Junction_Logout/Page_Login - Software to Manage Blinds from_4df230/td_Company  NameUser NamePasswordForgot pas_5ec4e4.rs
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>td_Company  NameUser NamePasswordForgot pas_5ec4e4</name>
+   <tag></tag>
+   <elementGuidId>011768b7-33d7-42a0-89a8-83f4a9239f0e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//tr[2]/td</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>td</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>align</name>
+      <type>Main</type>
+      <value>center</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+			
+			
+								
+					
+					
+					
+					
+					
+																								Company  Name
+						
+							
+						User Name
+												
+						
+						Password
+						
+												
+						
+							
+						
+												
+						Forgot password?
+											
+				
+			
+						
+				 
+					
+					
+					
+					
+					 
+					
+																								Company  Name
+						
+							
+						User Name
+												
+						
+						Email Address
+						
+												
+						
+							
+							
+						 
+						
+						Return to login
+					
+				
+			
+							</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;loginContainer&quot;]/table[1]/tbody[1]/tr[2]/td[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//tr[2]/td</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/JunctionLogin-Finaltesting/Script1601970369904.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('', FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.maximizeWindow(FailureHandling.OPTIONAL)
+
+WebUI.navigateToUrl('https://blindjunction.co.uk/finaltesting/', FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.setText(findTestObject('Object Repository/Junction_Login/Page_Login - Software to Manage Blinds from_4df230/input_Company  Name_company_name'), 
+    'DONSMITH')
+
+WebUI.setText(findTestObject('Object Repository/Junction_Login/Page_Login - Software to Manage Blinds from_4df230/input_User Name_user_name'), 
+    'admin')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Junction_Login/Page_Login - Software to Manage Blinds from_4df230/input_Password_user_password'), 
+    '4nvbrPglk7k=')
+
+WebUI.click(findTestObject('Object Repository/Junction_Login/Page_Login - Software to Manage Blinds from_4df230/input_Password_submitButton'))
+
+WebUI.waitForPageLoad(5)
+
+WebUI.getWindowTitle()
+
